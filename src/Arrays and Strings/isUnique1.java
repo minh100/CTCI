fe258eac85012f04890b0aff7b85bbf8dc243bca
minh100 <minh100@live.unc.edu>
2020-10-25 19:31:31 -0400

    first commit

---
+++ src/Arrays and Strings/isUnique1.java
@@ -0,0 +1,19 @@
+public class isUnique1 {
+
+  public static boolean isUniqueChars(String str){
+    boolean[] unique = new boolean[128];
+
+    for(int i = 0 ; i < str.length(); i++){
+      char value = str.charAt(i);
+      if(unique[value]){
+        return false;
+      }
+      unique[value] = true;
+    }
+    return true;
+  }
+
+  public static void main(String[] args){
+    System.out.println(isUniqueChars("hello"));
+  }
+}
